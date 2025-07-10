809754460062c3af07528a8217c04b12906b066e
a21jeaha <hisui86@gmail.com>
2025-07-10 10:13:18 +0200

    added GET method GetMovieFullDetails with documentation
    created ReviewDto, ActorDto, and MovieDetailDto for this request

---
+++ Models/DTOs/MovieDtos/MovieCreateDto.cs
@@ -0,0 +1,24 @@
+﻿using MovieApi.Models.DTOs.ActorDTOs;
+using MovieApi.Models.DTOs.MovieDetailsDTOs;
+using MovieApi.Models.DTOs.ReviewDTOs;
+using MovieApi.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApi.Models.DTOs.MovieDtos;
+
+public class MovieCreateDto
+{
+	[MaxLength(100)]
+	[Required(ErrorMessage = "The movie must have a name.")]
+	public string Title { get; set; } = null!;
+
+	[Range(1850, 9999)]
+	public int Year { get; set; }
+
+	[Range(5, 300)]
+	public int Duration { get; set; }
+	
+	[Required(ErrorMessage = "A movie must have a genre.")]
+	public int MovieGenreId { get; set; }
+}
