51f3374a2906ca4a1350f84583a17848a471bc59
a21jeaha <hisui86@gmail.com>
2025-07-10 10:15:25 +0200

    Update MovieDetails.cs
    removed unneeded code

--- Models/Entities/MovieDetails.cs
+++ Models/Entities/MovieDetails.cs
@@ -13,12 +13,6 @@ public class MovieDetails
 	public int MovieId { get; set; }
 	public string? Synopsis { get; set; } = null!;
 	public string? Language { get; set; } = null!;
-
-	/// <summary>
-	/// Gets or sets the Budget of the movie.
-	/// The Budget value must be between 0 and 500,000,000$.
-	/// </summary>
-	[Range(0, 500000000)]
 	public int Budget { get; set; }
 	public Movie Movie { get; set; } = null!;
 
