11663e2d623d9116fcfc810d2daa0a7e461da9c7
a21jeaha <hisui86@gmail.com>
2025-07-12 15:47:32 +0200

    added a base implementation of an entity repository

---
+++ MovieCore/DomainContracts/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using MovieCore.Models.Entities;
+
+namespace MovieCore.DomainContracts;
+
+public interface IRepositoryBase<T> where T : EntityBase
+{
+	//Task<IEnumerable<T>> GetAllAsync(bool trackChanges = false);
+	//Task<T> GetAsync(Expression<Func<T, bool>> expression, bool trackChanges = false);
+	//Task<bool> AnyAsync(int id);
+	void Add(T entity);
+	void Update(T entity);
+	void Remove(T entity);
+}
---
+++ MovieData/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.DomainContracts;
+using MovieCore.Models.Entities;
+using MovieData.Data;
+
+namespace MovieData.Repositories;
+
+internal class RepositoryBase<T> : IRepositoryBase<T> where T : EntityBase
+{
+	protected DbSet<T> DbSet { get; }
+
+	public RepositoryBase(MovieApiContext context)
+	{
+		DbSet = context.Set<T>();
+	}
+
+	public void Add(T entity) => DbSet.Add(entity);
+
+	public void Remove(T entity) => DbSet.Remove(entity);
+
+	public void Update(T entity) => DbSet.Update(entity);
+}
